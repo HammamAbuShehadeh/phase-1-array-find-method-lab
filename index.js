67c42bb840a2741ed4c7383319b91a62f829a287
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2023-10-05 07:54:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" },
+];
+const superbowlWin = (record) => {
+  for (let i = 0; i <= record.length - 1; i++) {
+    if (record[i].result === "W") {
+      return record[i].year;
+    } else {
+      continue;
+    }
+  }
+};
+record.find(superbowlWin);
